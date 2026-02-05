4cdff01ff985f1f40480cf43e766cc10c294b970
chris <christinekv01@gmail.com>
2026-02-05 19:22:59 +0100

    done done

--- Dynamic Numbers of Input Fields/dynamicNumbers.js
+++ Dynamic Numbers of Input Fields/dynamicNumbers.js
@@ -1,15 +1,36 @@
 var Linkless = document.getElementById("Less");
 var Linkmore = document.getElementById("More");
+var inputContainer = document.getElementById("input-container");
 
-function removeInput(){
-    document.addEventListener("click", function(){
-        input = document.getElementsByName("input");
+Linkless.addEventListener('click',removeInputField);
+Linkmore.addEventListener('click', addInputField);
+
+function addInputField(){
+        if (countInputs().length <= 4){
+        newfield = document.createElement('input');
+
+        inputContainer.appendChild(newfield);
+        inputContainer.appendChild(document.createElement('br'));
         
-        for (i=0; i<5; i++){
-            
+        } else {
+                alert("No more than 5 fields allowed bitch, calm yourself!!!!!");
         }
-
-    });
-
 }
+
+
+function removeInputField(){
+        if (countInputs().length > 0){
+                lastInput = inputContainer.querySelector('input:last-of-type');
+                if (lastInput) lastInput.remove();
+                countInputs[countInputs.length-1].remove();
+        } else{
+                alert('bitch are you dumb, there are no fields to remove, wtf?!');
+        }
+}
+
+function countInputs(){
+        return document.querySelectorAll('input');
+}
+
+
 
