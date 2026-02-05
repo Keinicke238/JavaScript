b6f21ca2dbfd3fd52b0436292c83f6fe5099067d
chris <christinekv01@gmail.com>
2026-02-05 14:46:58 +0100

    idk what im doing lol

---
+++ Dynamic Numbers of Input Fields/dynamicNumbers.js
@@ -0,0 +1,15 @@
+var Linkless = document.getElementById("Less");
+var Linkmore = document.getElementById("More");
+
+function removeInput(){
+    document.addEventListener("click", function(){
+        input = document.getElementsByName("input");
+        
+        for (i=0; i<5; i++){
+            
+        }
+
+    });
+
+}
+
